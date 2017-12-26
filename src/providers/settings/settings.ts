7218bf0c1d47023fb987f9ea3500b156f0916924
vadimsky <vribak@gmail.com>
2017-12-26 09:21:15 +0200

    add settings page

---
+++ src/providers/settings/settings.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {Platform} from "ionic-angular";
+import {DocumentDirection} from "ionic-angular/platform/platform";
+
+/*
+  Generated class for the SettingsProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SettingsProvider {
+
+  constructor(
+    private platform: Platform,
+    public http: HttpClient,
+    private translate: TranslateService
+  ) {
+    console.log('Hello SettingsProvider Provider');
+  }
+  setHebrew() {
+    this.setLanguage('he', 'rtl');
+  }
+
+  setDefault() {
+    // Language and direction
+    this.setHebrew();
+  }
+
+  setLanguage(lang: string, direction: DocumentDirection) {
+    this.platform.setDir(direction, true);
+    this.translate.setDefaultLang(lang);
+    this.translate.use(lang);
+  }
+
+  currentLang () {
+    return this.translate.currentLang;
+  }
+}
